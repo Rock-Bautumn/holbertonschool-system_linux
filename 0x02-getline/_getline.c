6c442e6c016e60989e39e8984b2d744b9d0a8593
Rock-Bautumn <3623@holbertonschool.com>
2022-05-17 23:13:05 -0500

    The starting point for task 1 in 0x02

---
+++ 0x02-getline/_getline.c
@@ -0,0 +1,13 @@
+#include "_getline.h"
+
+/**
+ * _getline - returns a string delimited by the newline character from a file
+ * @fd: the file descriptor of the file
+ * Return: the string, now newline char at the end
+*/
+
+char *_getline(const int fd)
+{
+	(void) fd;
+	return (NULL);
+}
