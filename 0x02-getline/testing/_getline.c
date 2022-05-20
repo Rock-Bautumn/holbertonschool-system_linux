e2e28656c0bbed5e23f0b406dd48310426903659
Rock-Bautumn <3623@holbertonschool.com>
2022-05-20 01:02:41 -0500

    closer but stuck on 1 0x02

--- 0x02-getline/testing/_getline.c
+++ 0x02-getline/testing/_getline.c
@@ -9,24 +9,53 @@
 char *_getline(const int fd)
 {
 	char *buffer;
+	char *searched_string;
 	char *output_string;
 	int i;
-	static int bytes_read;
+	int these_bytes_read;
+	int bytes_read = 0;
 	static char leftovers[READ_SIZE];
-	
-	buffer = read(fd, buffer, READ_SIZE);
 
-	for (i = 0; i < READ_SIZE; i++)
+	(void) searched_string;
+	buffer = malloc(sizeof(char) * READ_SIZE);
+	do
 	{
-		if (buffer[i] == '\n')
-			{
-				return (i);
-			}
-		bytes_read += READ_SIZE;
-		 
-	}
-	
-	
+		these_bytes_read = read(fd, buffer, READ_SIZE);
+
+		for (i = 0; i < READ_SIZE && i < these_bytes_read; i++)
+		{
+			if (buffer[i] == '\n')
+				{
+					memcpy(leftovers, &buffer[i+1], (READ_SIZE - i));
+					output_string = malloc(sizeof(char) * bytes_read + i);
+					if (output_string == NULL)
+						return (NULL);
+					if (searched_string != NULL)
+					{
+						memcpy(output_string, searched_string, bytes_read);
+						memcpy(output_string + bytes_read + 1, buffer, i + 1);
+						free(buffer);
+						return (output_string);
+					}
+					else
+					{
+						memcpy(output_string, buffer, i + 1);
+						free(buffer);
+						return (output_string);
+					}
+				}
+		}
+		if (these_bytes_read == READ_SIZE)
+		{
+			bytes_read += READ_SIZE;
+			if (bytes_read == READ_SIZE)
+				searched_string = malloc(sizeof(char) * READ_SIZE);
+			else
+				searched_string = realloc(searched_string, sizeof(char) * bytes_read);
+		}
+	} while (these_bytes_read > 1);
+	if (i > 0)
+		return buffer;
 	free(buffer);
 	return (NULL);
 }
--- 0x02-getline/testing/_getline.h
+++ 0x02-getline/testing/_getline.h
@@ -3,6 +3,9 @@
 #define READ_SIZE (1024)
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 char *_getline(const int fd);
 
