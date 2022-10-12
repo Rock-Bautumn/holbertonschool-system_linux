ba2d10d5537c694e63fcc798940e0f19b32fe822
Rock-Bautumn <3623@holbertonschool.com>
2022-10-11 20:13:30 -0500

    Adding working python function

--- 0x01-proc_filesystem/read_write_heap.py
+++ 0x01-proc_filesystem/read_write_heap.py
@@ -11,8 +11,8 @@ Usage:
 ./read_write_heap.py <process ID> <search string> <replacement string>
 """
 
-from multiprocessing import heap
 from sys import argv
+
 
 def get_heap_addrs(pid):
     """
@@ -45,17 +45,23 @@ def replace_memory(pid, heap_start, heap_end, search_string, rep_string):
     that search string with the replacement string
     Usage: get_heap_addrs(<process id>)
     """
+
+    heap_start = int(heap_start, 16)
+    heap_end = int(heap_end, 16)
+    written = 0
     try:
         with open("/proc/{:d}/mem".format(pid), "r+b") as file:
             print("Searching for " + search_string)
             file.seek(heap_start)
-            heap = file.read(heap_end - heap_start)
-            position = heap.find(search_string.encode())
+            heapy = file.read(heap_end - heap_start)
+            position = heapy.find(search_string.encode())
             if position > -1:
                 location = heap_start + position
-                print("Found string address at {:02X}").format(location)
+                print("Found string address!")
                 file.seek(location)
+                print("Now replacing the string...")
                 written = file.write(rep_string.encode() + b'\x00')
+                print("Wrote {:d} bytes".format(written))
             else:
                 print("\n\nThe string was not found.")
                 exit
@@ -64,10 +70,12 @@ def replace_memory(pid, heap_start, heap_end, search_string, rep_string):
         exit
 
 if __name__ == "__main__":
-    """The entry point to the script. Usage ./read_write_heap.py <PID> <search string> <replacement string>"""
+    """
+    The entry point to the script.
+    Usage ./read_write_heap.py <PID> <search string> <replacement string>
+    """
     if len(argv) < 4 or len(argv[3]) > len(argv[2]):
         print(__doc__)
         exit
     heap_start, heap_end = get_heap_addrs(int(argv[1]))
     replace_memory(int(argv[1]), heap_start, heap_end, argv[2], argv[3])
-
