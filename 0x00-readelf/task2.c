3cd516ebb75150101d77a8a0e8e205cb407a2f39
Rock-Bautumn <3623@holbertonschool.com>
2022-10-19 23:46:18 -0500

    task 2 ubuntu64 should be mostly done

--- 0x00-readelf/task2.c
+++ 0x00-readelf/task2.c
@@ -32,12 +32,59 @@ char *phtype_to_str64(Elf64_Word phtype)
 	return (NULL);
 }
 
+char *phflag_to_str64(Elf64_Word phflag, char *phtype)
+{
+	memcpy(phtype, "   ", 4);
+	if (phflag & PF_R)
+		phtype[0] = 'R';
+	if (phflag & PF_W)
+		phtype[1] = 'W';
+	if (phflag & PF_X)
+		phtype[2] = 'E';
+	return phtype;
+}
+
+void print_segments64(char *p)
+{
+	Elf64_Ehdr *ehdr = (Elf64_Ehdr*)p;
+	Elf64_Phdr *phdr = (Elf64_Phdr *)(p + ehdr->e_phoff);
+	Elf64_Shdr *shdr = (Elf64_Shdr *)(p + ehdr->e_shoff);
+	int phnum = ehdr->e_phnum;
+	int shnum = ehdr->e_shnum;
+	Elf64_Shdr *sh_strtab = &shdr[ehdr->e_shstrndx];
+	const char *const sh_strtab_p = p + sh_strtab->sh_offset;
+	int i = 0, j = 0;
+	Elf64_Off pl, ph, sl, sh;
+
+	printf("\n Section to Segment mapping:\n  Segment Sections...\n");
+	(void) phdr;
+	(void) sh_strtab_p;
+
+	for (i = 0; i < phnum; ++i)
+	{
+		pl = phdr[i].p_offset;
+		ph = phdr[i].p_offset + phdr[i].p_memsz;
+		printf("   %02d     ", i);
+		for (j = 0; j < shnum; ++j)
+		{
+			if (shdr[j].sh_addr == 0 || shdr[j].sh_size == 0)
+				continue;
+			sl = shdr[j].sh_offset;
+			sh = shdr[j].sh_offset + shdr[j].sh_size;
+			if (pl <= sl && sh <= ph)
+				printf("%s ", sh_strtab_p + shdr[j].sh_name);
+		}
+		printf("\n");
+	}
+}
+
 int print_phdr64(char *p) {
 	Elf64_Ehdr *ehdr = (Elf64_Ehdr*)p;
 	Elf64_Phdr *phdr = (Elf64_Phdr *)(p + ehdr->e_phoff);
 	Elf64_Off phoff = ehdr->e_phoff;
 	int phnum = ehdr->e_phnum;
 	int i = 0;
+	char phtype[4] = "   \0";
 
 	printf("\nElf file type is %s\n", "EXEC (Executable file)");
 	printf("Entry point 0x400600\nThere are ");
@@ -53,12 +100,16 @@ int print_phdr64(char *p) {
 		printf("0x%016lx ", phdr[i].p_paddr);
 		printf("0x%06lx ", phdr[i].p_filesz);
 		printf("0x%06lx ", phdr[i].p_memsz);
-		printf("%s ", "RWE");
+		printf("%s ", phflag_to_str64(phdr[i].p_flags, phtype));
 		printf("0x%lx", phdr[i].p_align);
-				
 		printf("\n");
+		if (phdr[i].p_type == PT_INTERP)
+		{
+			printf("      [Requesting program interpreter: ");
+			printf("%s]\n", (char *) (phdr[i].p_offset + p));
+		}
 	}
-
+	print_segments64(p);
 	return 0;
 }
 
