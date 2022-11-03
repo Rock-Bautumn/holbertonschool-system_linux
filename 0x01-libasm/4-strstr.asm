bdbf08785633b32b2e2d362c1a9746beaec4f2e8
Rock-Bautumn <3623@holbertonschool.com>
2022-11-02 22:02:46 -0500

    Adding task 4 to libasm

---
+++ 0x01-libasm/4-strstr.asm
@@ -0,0 +1,54 @@
+BITS 64
+
+global	asm_strstr
+	; char *strchr(char *string, char *string2);
+    ; rdi = char *string
+    ; rsi = char *string2
+
+asm_strstr:
+    push r8
+    push r9
+    push r10
+    push rcx
+    push rdx
+
+search_start:
+    movzx rax, BYTE [rdi]
+    cmp rax, BYTE 0
+    jz no_match
+    movzx rdx, BYTE [rsi]
+    cmp rax, rdx
+    je find_match
+
+    inc rdi
+    jmp search_start
+
+find_match:
+    mov r8, rsi
+    mov r9, rdi
+match_string:
+    cmp [r8], BYTE 0
+    jz match_found
+    inc r8
+    inc r9
+    movzx r10, BYTE [r8]
+    movzx rcx, BYTE [r9]
+    cmp r10, rcx
+    je match_string
+    jmp no_match
+
+no_match:
+    xor rax, rax
+    jmp cleanup
+
+match_found:
+    mov rax, rdi
+    jmp cleanup
+
+cleanup:
+    pop rdx
+    pop rcx
+    pop r10
+    pop r9
+    pop r8
+    ret
