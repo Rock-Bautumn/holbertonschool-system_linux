0a2b693c86f86b3c018503036c1d4031f1230074
Rock-Bautumn <3623@holbertonschool.com>
2022-11-03 22:08:53 -0500

    Adding task 11 strcspn to libasm

---
+++ 0x01-libasm/11-strcspn.asm
@@ -0,0 +1,52 @@
+BITS 64
+
+global asm_strcspn
+
+; size_t asm_strcspn(const char *s, const char *accept);
+
+; Count how many successive characters in s are NOT found in accept, from the beginning
+
+; rdi = char *s
+; rsi = char *accept
+; rcx = successful iterations, return value
+; rdx/dl = s char in question
+; rbx/bl = accept char in question
+; r9 = accept char address position
+
+asm_strcspn:
+    push rcx
+    push rdx
+    push rbx
+    push r9
+    xor rdx, rdx
+    xor rcx, rcx
+    xor rbx, rbx
+
+zcompare_char:
+    mov dl, [rdi + rcx]
+    cmp dl, 0
+    jz quit
+    xor r9, r9
+zsearch_char:
+    mov bl, [rsi + r9]
+    cmp bl, 0
+    jz znew_compare
+    cmp dl, bl
+    je quit
+    jmp znew_search
+
+znew_compare:
+    inc rcx
+    jmp zcompare_char
+
+znew_search:
+    inc r9
+    jmp zsearch_char
+
+quit:
+    mov rax, rcx
+    pop r9
+    pop rbx
+    pop rdx
+    pop rcx
+    ret
