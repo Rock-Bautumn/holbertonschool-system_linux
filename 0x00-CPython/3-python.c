a44874cf3cbd4d6a88761ceb06fd823399892896
Rock-Bautumn <3623@holbertonschool.com>
2022-12-05 20:38:32 -0600

    Replacing PyList_Size with PyList_GET_SIZE in task 3 0x01cpython

--- 0x00-CPython/3-python.c
+++ 0x00-CPython/3-python.c
@@ -92,7 +92,7 @@ void print_python_list(PyObject *p)
 	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
 	printf("[*] Allocated = %ld\n", list->allocated);
 
-	for (i = 0; i < PyList_Size(p); i++)
+	for (i = 0; i < PyList_GET_SIZE(p); i++)
 	{
 		listItem = PyList_GET_ITEM(p, i);
 		printf("Element %d: %s\n", i, listItem->ob_type->tp_name);
