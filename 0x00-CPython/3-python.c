592fc362a6713dcc3f0e8f5c7219eca1090c9e77
Rock-Bautumn <3623@holbertonschool.com>
2022-12-05 20:40:03 -0600

    switching the other PyList_Size for PyList_GET_SIZE in 3 0x01cpython

--- 0x00-CPython/3-python.c
+++ 0x00-CPython/3-python.c
@@ -89,7 +89,7 @@ void print_python_list(PyObject *p)
 		return;
 	}
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Size of the Python List = %ld\n", PyList_GET_SIZE(p));
 	printf("[*] Allocated = %ld\n", list->allocated);
 
 	for (i = 0; i < PyList_GET_SIZE(p); i++)
