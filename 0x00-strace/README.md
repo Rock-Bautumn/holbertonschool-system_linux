6c472117639117632de3944d53187e37d32d0e3a
Rock-Bautumn <3623@holbertonschool.com>
2023-01-10 20:16:45 -0600

    Adding task 0, initial commit for 0x00 strace

---
+++ 0x00-strace/README.md
@@ -0,0 +1,3 @@
+0x00-strace
+
+This project is about learning how to use ptrace to make our own version of strace.
---
+++ 0x00-strace/task0.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <unistd.h>
+
+#include "syscalls.h"
+
+/** fatal_error - ends the process while printing an error
+ * @error_text: the message to print when exiting
+ * Return: void
+*/
+
+void fatal_error(char *error_text)
+{
+	perror(error_text);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * main - Forks a child process that is ptraced
+ * @argc: The number of args
+ * @argv: The filepath of the executable to possess our child
+ * @environ: the environment of our process
+ * Return: void
+ */
+
+int main(int argc, char **argv, char **environ)
+{
+	pid_t pid;
+	struct user_regs_struct user_regs;
+	int child_status = EXIT_FAILURE, exit_status = EXIT_SUCCESS;
+	size_t alternator = 0;
+
+	if (argc < 2)
+		fatal_error("No program provided");
+	pid = fork();
+	if (pid == -1)
+		fatal_error("Fork failed");
+	else if (pid > 0)
+	{
+		puts("59");
+		while (!WIFEXITED(child_status))
+		{
+			alternator++;
+			ptrace(PTRACE_SYSCALL, pid, 0, 0);
+			wait(&child_status);
+if (ptrace(PTRACE_GETREGS, pid, 0, &user_regs) == 0 && alternator % 2 == 0)
+				printf("%lu\n", (size_t) user_regs.orig_rax);
+		fflush(stdout);
+		}
+	}
+	else if (pid == 0)
+	{
+		ptrace(PTRACE_TRACEME, pid, 0, 0);
+		execve(argv[1], &argv[1], environ);
+	}
+
+	return (exit_status);
+}
